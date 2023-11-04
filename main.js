7708b7edde782427363e4227e248aa3c0c53adbb
Aishen <aish3n@pm.me>
2023-11-04 15:12:48 +0800

    feat(shelter): add shelter page

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const routes = {
     404: "/pages/404.html",
     "/": "/pages/index.html",
     "/about": "/pages/about.html",
+    "/shelter": "/pages/shelter.html",
 };
 
 function route(event) {
