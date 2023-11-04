08b2e702db508e9c3cdb9fc00821a7105584057a
Aishen <aish3n@pm.me>
2023-11-04 11:14:42 +0800

    feat(all): simple routing

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// fb page: https://www.facebook.com/profile.php?id=61552656061343
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/index.html",
+    "/about": "/pages/about.html",
+};
+
+function route(event) {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    load();
+}
+
+async function load() {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route);
+
+    document.getElementById("main").innerHTML = await html.text();
+}
+
+window.onpopstate = load;
+window.route = route;
+
+load();
